44deca205f34ceea0cbd2abb4251d978a5763ee1
Tan-Tian-Wen <758902874@qq.com>
2017-09-08 11:31:19 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ var gravity = 0.0;
 
 function setup() {
  cnv=createCanvas(400, 400);
- cnv.position(100,100)
+ cnv.position(50,50)
 	loadImage("assets/3.jpg", function(img) {
 		img.loadPixels();
 		for (var x = 0; x < imgWidth; x++) {
